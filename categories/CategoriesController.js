42250b40d02ecc47eb9663df76522934bdea3bf3
Fernando Medeiros <79485966+fernandomeddev@users.noreply.github.com>
2021-07-08 15:07:15 -0300

    Atualização do painel admin das (em construção)

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -4,19 +4,50 @@ const Category = require('./Category');
 const slugify = require("slugify");
 
 router.get("/admin/categories/new",(req, res) => {
-    res.render('admin/categories/new');
+    res.render("admin/categories/new");
 })
 
-router.get('/categories/save', (req, res) => {
+router.post("/categories/save", (req, res) => {
     var title = req.body.title;
     if(title != undefined){
+        
         Category.create({
-
             title: title,
             slug: slugify(title)
+        }).then(() =>{
+            res.redirect("/");
         })
-    }else
-        res.redirect("/admin/categories/new")
+    }else{
+        res.redirect("/admin/categories/new");
+    }
 });
+
+router.get("/admin/categories", (req, res) => {
+
+    Category.findAll().then(categories => {
+        res.render("admin/categories/index", {
+            categories: categories});
+    });
+});
+
+router.post("/categories/delete", (req, res)=>{
+    var id = req.body.id;
+    if(id != undefined){
+        if(!isNaN(id)){
+
+            Category.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() =>{
+                res.redirect("/admin/categories");
+            });
+        }else{
+            res.redirect("/admin/categories");
+        }
+    }else{
+        res.redirect("/admin/categories")
+    }
+})
 
 module.exports = router;
