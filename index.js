b9555bb30868ce8b7a25be0f356975a1e3bdfee8
Fernando Medeiros <79485966+fernandomeddev@users.noreply.github.com>
2021-07-13 09:12:03 -0300

    Atualizando os Articles as Categories e o HomeNav

--- index.js
+++ index.js
@@ -55,8 +55,10 @@ app.get("/:slug", (req,res) => {
         }
     }).then(article => {
         if(article != undefined){
+            Category.findAll().then(categories => {
+                res.render("article", {article: article, categories: categories});
+            })
             
-            res.render("article", {article: article});
 
         }else{
             res.redirect("/");
@@ -67,6 +69,27 @@ app.get("/:slug", (req,res) => {
 })
 
 
+app.get("/category/:slug", (req, res)=> {
+    var slug = req.params.slug;
+    Category.findOne({
+        where:{
+            slug:slug
+        },
+        include:[{model: Article}]
+    }).then( category => {
+        if(category != undefined){
+
+            Category.findAll().then(categories => {
+                res.render("index",{articles:category.articles, categories: categories})
+            })
+
+        }else{
+            res.redirect("/");
+        }
+    }).catch( err => {
+        res.redirect("/")
+    })
+})
 
 
 app.listen(8080, () => {
